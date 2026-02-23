01bd23028eea8f5477766757fc2e79632cd94cd0
thakurshaurya <thakurshaurya2007@gmail.com>
2026-02-23 16:49:00 +0530

    Changed the text in the main card

--- src/components/Maincard.jsx
+++ src/components/Maincard.jsx
@@ -5,15 +5,18 @@ function Maincard(props) {
         <>
             <div className="h-100 p-px rounded-xl bg-black/60 backdrop-blur-md border-b border-purple-900/40 overflow-y-scroll">
                 <h1 className="text-5xl text-white m-5">Recent Notes</h1>
-                {props.task?.map((elem) => (
-                    <Card
-                        key={elem.id}
-                        id={elem.id}
-                        Notes={elem.notes}
-                        details={elem.detail}
-                        onDelete={props.onDelete}
-                    />
-                ))}
+                <div className="flex flex-col-reverse">
+                    {props.task?.map((elem) => (
+                        <Card
+                            key={elem.id}
+                            id={elem.id}
+                            Notes={elem.notes}
+                            details={elem.detail}
+                            onDelete={props.onDelete}
+                        />
+                    ))}
+
+                </div>
 
             </div>
         </>
